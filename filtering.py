f97789a6ecf5121eedcbec9601a23897f2188563
Jernej Virag <jernej.virag@gmail.com>
2011-12-29 16:52:00 +0100

    Implemented decimation

--- filtering.py
+++ filtering.py
@@ -1,15 +1,19 @@
+import multiprocessing
 from scipy import signal
 import scipy.io.wavfile
-import numpy as np
-
-from filter_plot import mfreqz
-from spectrogram.audio_demo import spectrogram, display_spectrogram
+import itertools
+import numpy
 
 def remove_channel(samples):
     return samples[:, 0]
 
+def _do_filter(args):
+    samples, fir_window = args
+    filtered_samples = scipy.signal.lfilter(fir_window,1,samples)
+    return filtered_samples
+
 def band_pass_filter(samples, samplerate, low_hz, high_hz):
-    taps = 201
+    taps = 61
     cutoff_low = float(low_hz)
     cutoff_high = float(high_hz)
     nyquist = samplerate / 2.0
@@ -18,15 +22,31 @@ def band_pass_filter(samples, samplerate, low_hz, high_hz):
                                cutoff=[cutoff_low / nyquist, cutoff_high / nyquist],
                                pass_zero=False,
                                window="blackman")
-    filtered_samples = scipy.signal.lfilter(fir_window,1,samples)
-    filtered_samples = filtered_samples.astype(samples.dtype)
 
+    # Split samples in no. of processor subarrays
+    samples_list = numpy.array_split(samples, multiprocessing.cpu_count())
+    worker_pool = multiprocessing.Pool(processes=multiprocessing.cpu_count())
+    results = worker_pool.map(_do_filter, itertools.izip(samples_list, itertools.repeat(fir_window)))
+    filtered_samples = numpy.concatenate(results).astype(samples.dtype)
+    print "Results: ", filtered_samples
     return filtered_samples
 
+def downsample(samples, samplerate, max_frequency):
+    # First find minimum samplerate multiplier to satisfy nyquist
+    q = 1
+    while samplerate / (q + 1) > (max_frequency * 2):
+        q += 1
+
+    print "Found decimation factor: ", q
+    decimated_samples = signal.decimate(samples, q).astype(samples.dtype)
+    return decimated_samples, samplerate / q
+
 if __name__ == "__main__":
-    (samplerate, samples) = scipy.io.wavfile.read("file-c.wav")
+    (samplerate, samples) = scipy.io.wavfile.read("liberatio.wav")
     if samples[0].size > 1:
         samples = remove_channel(samples)
 
     filtered_audio = band_pass_filter(samples, samplerate, 400, 3000)
+    filtered_audio,samplerate = downsample(filtered_audio, samplerate, 3000)
     scipy.io.wavfile.write("filtered.wav", samplerate, filtered_audio)
+
